84b1e3b3bcfa15444f5fbc4fc30a7b7fd99df6c9
DrewAPicture <info@drewapicture.com>
2017-12-05 10:15:08 -0700

    Use a more descriptive summary for cache.

--- src/Cache_Command.php
+++ src/Cache_Command.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Manipulates the WP Object Cache object.
+ * Adds, removes, fetches, and flushes the WP Object Cache object.
  *
  * By default, the WP Object Cache exists in PHP memory for the length of the
  * request (and is emptied at the end). Use a persistent object cache drop-in
