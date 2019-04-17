16859c8124ab7ab1f8ae435666e9ccd69d730f7e
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-17 14:40:24 +0200

    Prefix variables in global namespace

--- cache-command.php
+++ cache-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wp_cli_cache_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wp_cli_cache_autoloader ) ) {
+	require_once $wp_cli_cache_autoloader;
 }
 
 WP_CLI::add_command( 'cache', 'Cache_Command' );
